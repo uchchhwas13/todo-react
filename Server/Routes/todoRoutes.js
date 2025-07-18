3673b6c65f5edd725a21be8341f3641aeec50401
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-18 12:05:11 +0600

    Updating route path

--- Server/Routes/todoRoutes.js
+++ Server/Routes/todoRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../Controllers/todoController');
 
-router.get('/todos', todoController.getTodos);
-router.post('/todos', todoController.addTodo);
-router.put('/todos/:id', todoController.updateTodo);
-router.delete('/todos/:id', todoController.deleteTodo);
+router.get('/', todoController.getTodos);
+router.post('/', todoController.addTodo);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
 
 module.exports = router;
