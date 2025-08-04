25310f92cf6d4f68a3828240e4d291d6aa46571e
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-04 18:56:19 +0600

    Converting routes from JS to TS

---
+++ Server/src/Routes/todoRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import {
+  getTodos,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+} from '../Controllers/todoController';
+
+const router = Router();
+router.get('/', getTodos);
+router.post('/', addTodo);
+router.put('/:id', updateTodo);
+router.delete('/:id', deleteTodo);
+
+export default router;
