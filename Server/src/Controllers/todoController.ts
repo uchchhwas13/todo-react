7d104c2de201ed63d0948cdaa12fb3b4ab1f8637
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-06 17:00:32 +0600

    Adding type for response

--- Server/src/Controllers/todoController.ts
+++ Server/src/Controllers/todoController.ts
@@ -1,12 +1,38 @@
 import type { Request, Response } from 'express';
 import { ToDoModel } from '../Models/Todo';
 import { z } from 'zod';
+import { ITodo } from '../Models/Todo';
 import { addTodoSchema, updateTodoSchema } from '../Validators/toDoValidators';
 
 type AddTodoRequestBody = z.infer<typeof addTodoSchema>;
 type UpdateTodoRequestBody = z.infer<typeof updateTodoSchema>;
 
-export const getTodos = async (req: Request, res: Response) => {
+type TodosGetSuccessResponse = {
+  todos: ITodo[];
+};
+
+type ErrorResponse = {
+  error: string;
+};
+
+type TodoPostSuccessResponse = {
+  message: string;
+  result: ITodo;
+};
+
+type TodoPutSuccessResponse = {
+  message: string;
+  updatedTodo: ITodo;
+};
+
+type TodoDeleteSuccessResponse = {
+  message: string;
+};
+
+export const getTodos = async (
+  req: Request,
+  res: Response<TodosGetSuccessResponse | ErrorResponse>
+) => {
   try {
     const todos = await ToDoModel.find({});
     return res.status(200).json({ todos });
@@ -18,7 +44,7 @@ export const getTodos = async (req: Request, res: Response) => {
 
 export const addTodo = async (
   req: Request<{}, {}, AddTodoRequestBody>,
-  res: Response
+  res: Response<TodoPostSuccessResponse | ErrorResponse>
 ): Promise<Response> => {
   const { id, text, isComplete } = req.body.todo;
   try {
@@ -32,21 +58,23 @@ export const addTodo = async (
 
 export const updateTodo = async (
   req: Request<{ id: string }, {}, UpdateTodoRequestBody>,
-  res: Response
+  res: Response<TodoPutSuccessResponse | ErrorResponse>
 ) => {
   const { todo } = req.body;
 
   try {
-    const updated = await ToDoModel.findOneAndUpdate(
+    const updatedTodo = await ToDoModel.findOneAndUpdate(
       { id: req.params.id },
       { $set: todo },
       { new: true, runValidators: true }
     );
 
-    if (!updated) {
+    if (!updatedTodo) {
       return res.status(404).json({ error: 'Todo not found' });
     }
-    return res.status(200).json(updated);
+    return res
+      .status(200)
+      .json({ message: 'Todo updated successfully', updatedTodo });
   } catch (error) {
     console.error('Update error:', error);
     return res.status(500).json({ error: 'Failed to update todo' });
@@ -55,7 +83,7 @@ export const updateTodo = async (
 
 export const deleteTodo = async (
   req: Request<{ id: string }, {}, {}>,
-  res: Response
+  res: Response<TodoDeleteSuccessResponse | ErrorResponse>
 ) => {
   try {
     const deleted = await ToDoModel.findOneAndDelete({ id: req.params.id });
