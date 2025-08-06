68659e662c1848ddae24e342b7d15516714be9cf
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-06 15:05:52 +0600

    Adding a todo validator zod schema

---
+++ Server/src/Validators/toDoValidators.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const todoSchema = z.object({
+  id: z.string(),
+  text: z.string(),
+  isComplete: z.boolean(),
+});
+
+export const addTodoSchema = z.object({
+  todo: todoSchema,
+});
