7aa86a6eebdcbaa26c488e0d158531cdcb96a921
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-06 18:25:01 +0600

    Updating TodoHeader to tsx

--- Client/src/components/TodoHeader.tsx
+++ Client/src/components/TodoHeader.tsx
@@ -1,6 +1,7 @@
 import todo_icon from '../assets/todo_icon.png';
+import { FC } from 'react';
 
-const TodoHeader = () => (
+const TodoHeader: FC = () => (
   <div className="flex items-center mt-7 gap-2">
     <img src={todo_icon} alt="To-Do icon" className="w-8" />
     <h1 className="text-3xl font-semibold">To-DO List</h1>
